43d380d51d3d2ebb6e4e3e1526df82a010e6f1bc
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-04 19:57:51 +0530

    Fix formatting issues in widget_config.dart by removing extra blank lines and adding a newline at the end of the file

--- lib/widgets/common/widget_config.dart
+++ lib/widgets/common/widget_config.dart
@@ -4,21 +4,21 @@ class WidgetConfig {
   static const double buttonHeight = 48.0;
   static const double buttonPadding = 16.0;
   static const double buttonRadius = 12.0;
-  
+
   // Card configurations
   static const double cardElevation = 4.0;
   static const double cardRadius = 12.0;
   static const double cardPadding = 16.0;
-  
+
   // Input field configurations
   static const double inputRadius = 12.0;
   static const double inputPadding = 16.0;
-  
+
   // Spacing
   static const double smallSpacing = 8.0;
   static const double mediumSpacing = 16.0;
   static const double largeSpacing = 24.0;
-  
+
   // Icon sizes
   static const double smallIcon = 16.0;
   static const double mediumIcon = 24.0;

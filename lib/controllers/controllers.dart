c1b0b9060b51e07c233bee685f2d3855dcd522ed
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-04 19:55:50 +0530

    Reorder controller exports in controllers.dart for better organization

--- lib/controllers/controllers.dart
+++ lib/controllers/controllers.dart
@@ -1,6 +1,6 @@
 // Export all controllers
+export 'auction_controller.dart';
 export 'auth_controller_simple.dart';
-export 'product_controller.dart';
 export 'cart_controller.dart';
 export 'order_controller.dart';
-export 'auction_controller.dart';
+export 'product_controller.dart';

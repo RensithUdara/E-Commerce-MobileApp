64a400079b0d77bf9fcab83b73ca34730767b8d0
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-04 19:58:59 +0530

    Fix formatting issues in route_manager.dart by removing extra blank lines and adding newlines for consistency

--- lib/config/route_manager.dart
+++ lib/config/route_manager.dart
@@ -2,45 +2,47 @@ import 'routes.dart';
 
 class RouteManager {
   static final Map<String, dynamic> _routeData = {};
-  
+
   // Navigation methods
   static void navigateTo(String routeName, {Map<String, dynamic>? arguments}) {
     _routeData[routeName] = arguments ?? {};
     // This will be implemented with actual Flutter navigation later
     print('Navigating to: $routeName with arguments: $arguments');
   }
-  
-  static void navigateAndReplace(String routeName, {Map<String, dynamic>? arguments}) {
+
+  static void navigateAndReplace(String routeName,
+      {Map<String, dynamic>? arguments}) {
     _routeData[routeName] = arguments ?? {};
     // This will be implemented with actual Flutter navigation later
     print('Navigate and replace to: $routeName with arguments: $arguments');
   }
-  
-  static void navigateAndClearStack(String routeName, {Map<String, dynamic>? arguments}) {
+
+  static void navigateAndClearStack(String routeName,
+      {Map<String, dynamic>? arguments}) {
     _routeData[routeName] = arguments ?? {};
     // This will be implemented with actual Flutter navigation later
     print('Navigate and clear stack to: $routeName with arguments: $arguments');
   }
-  
+
   static void goBack() {
     // This will be implemented with actual Flutter navigation later
     print('Going back');
   }
-  
+
   // Route data management
   static Map<String, dynamic>? getRouteData(String routeName) {
     return _routeData[routeName];
   }
-  
+
   static void clearRouteData(String routeName) {
     _routeData.remove(routeName);
   }
-  
+
   // Route validation
   static bool isValidRoute(String routeName) {
     return _getValidRoutes().contains(routeName);
   }
-  
+
   static List<String> _getValidRoutes() {
     return [
       AppRoutes.splash,
@@ -72,7 +74,7 @@ class RouteManager {
       AppRoutes.sellerNotifications,
     ];
   }
-  
+
   // Deep link handling
   static String? handleDeepLink(String url) {
     // Parse deep links and return appropriate route

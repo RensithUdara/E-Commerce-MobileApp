c7ccd105d0ca7ea7b05708e87cb23b27264e1616
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-08 01:12:35 +0530

    Fix formatting in buyer_views.dart for consistency

--- lib/views/buyer/buyer_views.dart
+++ lib/views/buyer/buyer_views.dart
@@ -2,18 +2,13 @@
 
 // Auction Views
 export 'auction/auction_views.dart';
-
-// Cart Views  
+// Cart Views
 export 'cart/cart_views.dart';
-
 // Home Views
 export 'home/home_views.dart';
-
 // Order Views
 export 'order/order_views.dart';
-
 // Product Views
 export 'product/product_views.dart';
-
 // Profile Views
 export 'profile/profile.dart';

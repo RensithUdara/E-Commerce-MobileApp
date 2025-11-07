0bfd10e8b80f9f50e699e0eb3b437509ee7c799a
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-08 01:08:18 +0530

    Fix import path in seller_notifications_view.dart for consistency

--- lib/views/seller/notifications/seller_notifications_view.dart
+++ lib/views/seller/notifications/seller_notifications_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../../widgets/common/loading_widget.dart';
+import '../../../widgets/common/loading_widget.dart';
 
 class SellerNotificationsView extends StatefulWidget {
   const SellerNotificationsView({super.key});

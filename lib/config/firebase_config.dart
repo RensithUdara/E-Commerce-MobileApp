12b0af0332edc584723f8ac4442ec64d28e18c56
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-11-08 01:50:00 +0530

    Add Firebase configuration for authentication and app check

---
+++ lib/config/firebase_config.dart
@@ -0,0 +1,38 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+
+class FirebaseConfig {
+  static Future<void> configureFirebaseAuth() async {
+    // Configure Firebase Auth settings for development
+    if (kDebugMode) {
+      await FirebaseAuth.instance.setSettings(
+        appVerificationDisabledForTesting: true,
+      );
+    }
+  }
+
+  static Future<void> configureAppCheck() async {
+    await FirebaseAppCheck.instance.activate(
+      // For Android
+      androidProvider: kDebugMode 
+          ? AndroidProvider.debug 
+          : AndroidProvider.playIntegrity,
+      
+      // For iOS  
+      appleProvider: kDebugMode 
+          ? AppleProvider.debug 
+          : AppleProvider.appAttest,
+      
+      // For web - you'll need to get a reCAPTCHA site key from Firebase Console
+      webProvider: kDebugMode 
+          ? ReCaptchaV3Provider('debug') 
+          : ReCaptchaV3Provider('your-production-recaptcha-site-key'),
+    );
+  }
+
+  static Future<void> initializeAll() async {
+    await configureAppCheck();
+    await configureFirebaseAuth();
+  }
+}
